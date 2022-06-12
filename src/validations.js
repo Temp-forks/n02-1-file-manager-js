864f7dfacb64901917f65722563c947f7ccb7ad9
Muhammed Abdrahman <muhammed03amin.com>
2022-06-12 21:35:09 +0600

    feat: add alert message on console

--- src/validations.js
+++ src/validations.js
@@ -3,6 +3,7 @@ import {messages, userParams} from "./settings.js";
 export const validateArgs = (args) => {
   if (args.length <= 2) {
     console.log(messages.invalidInput);
+    console.log('Please set up user correctly, type: "node src/index.js --username=USERNAME"');
     process.exit()
   }
 }
@@ -10,6 +11,7 @@ export const validateArgs = (args) => {
 export const validateUsername = (userParam) => {
   if (userParam !== '--username' || !userParams.username) {
     console.log(messages.invalidUsername);
+    console.log('Please set up user correctly, type: "node src/index.js --username=USERNAME"');
     process.exit();
   }
 }
