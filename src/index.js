21bafd9a6bd460d4fcfd1d83ab6bb1c997d229b3
Muhammed Abdrahman <muhammed03amin.com>
2022-06-12 14:58:14 +0600

    feat: add main operation loop

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('Welcome to file manager!')
+import { homedir } from 'os';
+import { createInterface } from 'readline'
+import {messages, userParams} from "./settings.js";
+import {validateArgs, validateUsername} from "./validations.js";
+import { parseOperation } from "./operations.js";
+
+const main = async () => {
+  console.log(`${messages.welcome} ${userParams.username}!`);
+  console.log(`${messages.currentPath} ${userParams.currentPath}`);
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.on('line', (line) => {
+    parseOperation(line.split(' '));
+    console.log(`${messages.currentPath} ${userParams.currentPath}`);
+  })
+  process.on('exit', () => {
+    rl.close()
+    console.log(`${messages.thank} ${userParams.username}!`)
+  })
+}
+
+const init = () => {
+  let userParam;
+  userParams.currentPath = homedir()
+  validateArgs(process.argv);
+  [userParam, userParams.username] = process.argv.slice(2)[0].split('=');
+  validateUsername(userParam);
+  main().then()
+}
+
+init()
