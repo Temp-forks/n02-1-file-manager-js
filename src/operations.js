108fc2ae7f0ad8e8f0fd3d22284bf5a04c239c30
Muhammed Abdrahman <muhammed03amin.com>
2022-06-12 16:01:46 +0600

    feat: add file operations

--- src/operations.js
+++ src/operations.js
@@ -1,5 +1,13 @@
-import {commands} from "./settings.js";
+import {commands, messages} from "./settings.js";
 import {goToFolder, goUpperDir, printListOfDir} from "./commands/nav/handleCommand.js";
+import {
+  copyFile,
+  createFile,
+  moveFile,
+  printFileContent,
+  removeFile,
+  renameFile
+} from "./commands/file/handleCommand.js";
 
 export const parseOperation = (operation) => {
   switch (operation[0]) {
@@ -12,5 +20,25 @@ export const parseOperation = (operation) => {
     case commands.ls:
       printListOfDir()
       break;
+    case commands.cat:
+      printFileContent(operation[1]);
+      break;
+    case commands.add:
+      createFile(operation[1]);
+      break;
+    case commands.rn:
+      renameFile(operation[1], operation[2]);
+      break;
+    case commands.cp:
+      copyFile(operation[1], operation[2]);
+      break;
+    case commands.mv:
+      moveFile(operation[1], operation[2]);
+      break;
+    case commands.rm:
+      removeFile(operation[1]);
+      break;
+    default:
+      console.log(messages.error);
   }
 }
